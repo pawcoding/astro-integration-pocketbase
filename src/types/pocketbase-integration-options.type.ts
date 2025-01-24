0cb3ee811ad88c4973b020128bd301a72278e78e
pawcode Development <development@pawcode.de>
2025-01-24 22:27:36 +0100

    feat(realtime): provide advanced settings for realtime subscriptions
    These advanced settings allow you to specify alias collections, that can
    trigger a live reload of another collection. This is especially useful
    when working with View Collections as they don't send realtime updates
    by themselves.
    
    pawcoding/astro-integration-pocketbase#10

--- src/types/pocketbase-integration-options.type.ts
+++ src/types/pocketbase-integration-options.type.ts
@@ -23,6 +23,28 @@ export interface PocketBaseIntegrationOptions {
   /**
    * List of PocketBase collections to watch for changes.
    * When an entry in one of these collections changes, the content will be reloaded.
+   *
+   * Example:
+   * ```ts
+   * collectionsToWatch: ["users", "posts"]
+   * ```
+   *
+   * For advanced usage, you can specify a map where the key is the collection that is used to load the content.
+   * The value can be:
+   * - `true` to watch the collection itself for changes (base collection)
+   * - an array of strings to watch multiple collections for changes (view collection)
+   *
+   * These advanced options are especially useful when you're working with view collections.
+   * [View collections](https://pocketbase.io/docs/collections/#view-collection) don't emit events when their
+   * entries change, since they are based on other collections.
+   *
+   * Example:
+   * ```ts
+   * collectionsToWatch: {
+   *  "users": true,
+   *  "postings": ["posts", "comments"]
+   * }
+   * ```
    */
-  collectionsToWatch?: Array<string>;
+  collectionsToWatch?: Array<string> | Record<string, true | Array<string>>;
 }
