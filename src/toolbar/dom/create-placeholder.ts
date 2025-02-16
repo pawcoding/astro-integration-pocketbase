06e31f8424b68d3116e06f0b10fe2b69ce2c1272
pawcode Development <development@pawcode.de>
2025-02-16 18:46:11 +0100

    refactor(toolbar): use HTML and CSS in template literal
    Instead of building the toolbar elements imperatively, it is now
    declared in HTML and CSS inside of template literals. This makes it much
    easier to add layout elements or add styling via CSS.
    
    The toolbar now also uses internal functions by Astro to create the
    window element, to synchronize the window placement and to close the
    window on outside click. This removes some of the previous hacks and
    refactors the window to be more like the first party integrations.

--- src/toolbar/dom/create-placeholder.ts
+++ src/toolbar/dom/create-placeholder.ts
@@ -1,23 +1,22 @@
-import type { DevToolbarCard } from "astro/runtime/client/dev-toolbar/ui-library/card.js";
-
 /**
  * Creates a placeholder card.
  */
-export function createPlaceholder(): DevToolbarCard {
-  // Create the main card
-  const main = document.createElement("astro-dev-toolbar-card");
+export function createPlaceholder(): string {
+  return /* HTML */ `
+    <style>
+      #placeholder {
+        > div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    </style>
 
-  // Create the main content container
-  const content = document.createElement("div");
-  content.style.display = "flex";
-  content.style.alignItems = "center";
-  content.style.justifyContent = "center";
-  main.appendChild(content);
-
-  // Add the placeholder text
-  const placeholder = document.createElement("span");
-  placeholder.textContent = "Here you will see the raw content of an entity";
-  content.appendChild(placeholder);
-
-  return main;
+    <astro-dev-toolbar-card id="placeholder">
+      <div>
+        <span> Here you will see the raw content of an entity </span>
+      </div>
+    </astro-dev-toolbar-card>
+  `;
 }
