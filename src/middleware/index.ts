01f6bcfed77c6b22f621efde9269a5f89867d34f
pawcode Development <development@pawcode.de>
2025-03-30 15:43:41 +0200

    refactor(middleware): refactor code after merging PR

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,13 +4,11 @@ import { isPocketbaseEntry, type PocketBaseEntry } from "./is-pocketbase-entry";
 export const onRequest = defineMiddleware(async (context, next) => {
   // Look for entities given as props to the page
   const props = Object.values(context.props);
-
   const entities = findEntitiesRecursive(props).map((entity) => entity.data);
 
   const response = await next();
-
-  const contentType = response.headers.get("content-type") || "";
-  if (!contentType.includes("text/html")) {
+  const contentType = response.headers.get("content-type");
+  if (!contentType?.includes("text/html")) {
     // Pass through non-HTML responses unchanged
     return response;
   }
@@ -24,11 +22,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     `<script>window.__astro_entities__ = ${entitiesJson}</script></head>`
   );
 
-  return new Response(newBody, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: response.headers
-  });
+  return new Response(newBody, response);
 });
 
 /**
